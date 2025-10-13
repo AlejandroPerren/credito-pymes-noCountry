2e5ae161a4e59004d1e6ddb6fb33c7644d7786ec
Zoranspiegel <zoranbow@gmail.com>
2025-10-13 16:54:59 -0500

    How It Works minor fixes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
+import Benefits from "@/components/organism/Benefits";
 import HeroSection from "@/components/organism/HeroSection";
 import HowItWorks from "@/components/organism/HowItWorks";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-screen overflow-x-hidden">
       <HeroSection />
       <HowItWorks />
+      <Benefits />
     </div>
   );
 }
--- frontend/src/components/organism/HeroSection.tsx
+++ frontend/src/components/organism/HeroSection.tsx
@@ -4,7 +4,7 @@ import { ShieldCheck, Timer, Headset } from "lucide-react";
 
 export default function HeroSection() {
   return (
-    <div className="flex bg-popover px-brand-3xl py-brand-2xl">
+    <div className="flex gap-brand-lg bg-popover px-brand-3xl py-brand-2xl">
       <div className="w-full flex flex-col items-start justify-center gap-brand-lg">
         <div className="flex flex-col gap-brand-md">
           <h1 className="heading-1">La financiación que tu PYME necesita, simple y 100% digital.</h1>
@@ -12,8 +12,8 @@ export default function HeroSection() {
             Solicita créditos, sube tu documentación y recibe una respuesta en tiempo récord. Todo desde un solo lugar.
           </p>
         </div>
-        <Button size="lg">Label</Button>
-        <div className="flex gap-brand-sm">
+        <Button size="lg">Crear Cuenta</Button>
+        <div className="flex gap-brand-md">
           <div className="flex gap-brand-sm items-center">
             <ShieldCheck width={16} height={16} />
             <span className="paragraph-mini-medium">100% Seguro</span>
