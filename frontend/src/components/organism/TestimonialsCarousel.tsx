db31a12ed0e38559c7bf7c9a29314d4b9f321fc2
Zoranspiegel <zoranbow@gmail.com>
2025-10-14 11:06:29 -0500

    Testimonials Section Animated

--- frontend/src/components/organism/TestimonialsCarousel.tsx
+++ frontend/src/components/organism/TestimonialsCarousel.tsx
@@ -2,15 +2,18 @@
 
 import { TESTIMONIALS_ITEMS } from "@/utils/consts/testimonialsItems";
 import TestimonialsCard from "../molecules/TestimonialsCard";
+import { motion } from "motion/react";
 
 export default function TestimonialsCarousel() {
   return (
-    <div className="w-full overflow-hidden">
-      <div className="flex w-max gap-brand-lg p-brand-lg">
-        {[...TESTIMONIALS_ITEMS].map((item, i) => (
-          <TestimonialsCard key={i} client={item} />
-        ))}
-      </div>
-    </div>
+    <motion.div
+      className="flex w-max gap-brand-lg p-brand-lg"
+      animate={{ x: ["0%", "-50%"] }}
+      transition={{ duration: 30, ease: "linear", repeat: Infinity, repeatType: "loop", repeatDelay: 0 }}
+    >
+      {[...TESTIMONIALS_ITEMS, ...TESTIMONIALS_ITEMS].map((item, i) => (
+        <TestimonialsCard key={i} client={item} />
+      ))}
+    </motion.div>
   );
 }
