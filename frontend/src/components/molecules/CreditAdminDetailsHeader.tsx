d1ad826e12d7f32c3b876a50d4ab9db0459bd3c3
Zoranspiegel <zoranbow@gmail.com>
2025-10-29 11:46:15 -0500

    user profile page mocked

--- frontend/src/components/molecules/CreditAdminDetailsHeader.tsx
+++ frontend/src/components/molecules/CreditAdminDetailsHeader.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { useAdminContext } from "@/store/adminContext";
-import { notFound, useRouter } from "next/navigation";
-import { Button } from "../ui/button";
-import { ArrowLeft } from "lucide-react";
+import { notFound } from "next/navigation";
 import StatusLabel from "../atoms/StatusLabel";
+import RouterBackBtn from "../atoms/RouterBackBtn";
 
 export default function CreditAdminDetailsHeader({ id }: { id: number }) {
   const { state } = useAdminContext();
-  const router = useRouter();
 
   const credit = state.credits.find((credit) => credit.id === id);
 
@@ -16,10 +14,7 @@ export default function CreditAdminDetailsHeader({ id }: { id: number }) {
 
   return (
     <div className="flex gap-brand-lg">
-      <Button variant="ghost" onClick={() => router.back()}>
-        <ArrowLeft />
-        ...
-      </Button>
+      <RouterBackBtn />
 
       <div className="flex gap-brand-md">
         <div className="flex flex-col gap-brand-md">
